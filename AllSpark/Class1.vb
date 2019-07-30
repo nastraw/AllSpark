92dd09cef2c9cc032192c74c249c55cc0515a498
Nathan Straw <nstraw@drintl.com>
2019-07-29 23:13:34 -0400

    we did the things

---
+++ AllSpark/Class1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Room
+    Public Sub New(strRoomDesc As String)
+        Description = strRoomDesc
+
+    End Sub
+
+    Private Description As String = "room one"
+
+
+    Sub DisplayDescription()
+        Console.WriteLine(Description)
+
+    End Sub
+
+End Class
--- AllSpark/Module1.vb
+++ AllSpark/Module1.vb
@@ -1,12 +1,50 @@
 ﻿Module Module1
 
 	Sub Main()
-		Console.WriteLine("Hello World")
-		Console.WriteLine("Unstaged change")
 
-		Console.ReadKey()
+        Dim firstRoom As Room = New Room("first room")
+        Dim secondRoom As Room = New Room("the library")
+        Dim thirdRoom As Room = New Room("the billiards room")
+        Dim interror As Integer
+        Dim currentRoom As Integer = 0
+        Dim input As Integer
 
+        Do While currentRoom >= 0
 
-	End Sub
+            Console.WriteLine("what do you want to do: 0-move 1-inventory 2-explore")
+            input = Console.ReadLine
+
+            If input = 0 Then
+                Console.WriteLine("choose your path: 0-first room 1-library 2-billiards room")
+
+                interror = currentRoom
+                currentRoom = Console.ReadLine
+                If currentRoom = 0 Then
+                    firstRoom.DisplayDescription()
+                ElseIf currentRoom = 1 Then
+                    secondRoom.DisplayDescription()
+                ElseIf currentRoom = 2 Then
+                    thirdRoom.DisplayDescription()
+                ElseIf currentRoom < 0 Then
+                    Exit Do
+                Else
+                    Console.WriteLine("Invalid input")
+                    currentRoom = interror
+                End If
+            ElseIf input = 1 Then
+                'inventory
+            Else
+                'explore
+
+            End If
+
+        Loop
+
+        Console.WriteLine("Bye for now!")
+
+        Console.ReadKey()
+
+    End Sub
+
 
 End Module
