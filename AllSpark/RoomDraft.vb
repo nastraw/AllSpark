3530f18372cb1cf41ff2d8e594fe01f1379cdfe3
Kyle Courtemanche <kpcourtemanche@outlook.com>
2019-07-31 10:26:45 -0400

    This was an experiment to see how an array could be used to store/navigate through rooms. Happy with the result but still wonder if its the best/simplest approach for when things gets more complicated. See what you think

---
+++ AllSpark/RoomDraft.vb
@@ -0,0 +1,21 @@
+﻿Public Class RoomDraft
+
+
+    'all just part of the test to see about adding additional components
+
+    Public RoomText As String
+    Public RoomNum As Integer
+
+    Public Sub New(strCurrentRoomName As String, intRoomNum As Integer)
+        RoomText = strCurrentRoomName
+        RoomNum = intRoomNum
+
+    End Sub
+
+
+
+    '    Sub DisplayDescription()
+    '        Console.WriteLine(RoomText)
+
+    '   End Sub
+End Class
