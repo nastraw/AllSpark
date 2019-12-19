bed1c3a42001f410ea33ff5a0643fbdddf725596
Kyle Courtemanche <kpcourtemanche@outlook.com>
2019-12-19 18:42:19 -0500

    Created methods for organization/clarity. Updated options to start at 1 for error trapping with Val function. Worked on Locked Doors - added keys you find through explore and allow passage through a doorway.

---
+++ AllSpark/Module1 (Moses's conflicted copy 2019-08-19).vb
@@ -0,0 +1,144 @@
+﻿Module Module1
+
+	Sub Main()
+
+        'This was to check that we could have more than one component for a room and it works
+        'Dim alphaRoom As RoomDraft = New RoomDraft("the study", 12)
+        'Console.WriteLine(alphaRoom.RoomText)
+        'Console.WriteLine(alphaRoom.RoomNum)
+
+        Dim currentRoom As Integer = 0
+        Dim input As Integer
+        'Dim firstRoom As Room = New Room("first room")
+        'Dim secondRoom As Room = New Room("the library")
+        'Dim thirdRoom As Room = New Room("the billiards room")
+        'Dim interror As Integer
+        'Do While currentRoom >= 0
+
+        '    Console.WriteLine("what do you want to do: 0-move 1-inventory 2-explore")
+        '    input = Console.ReadLine
+
+        '    If input = 0 Then
+        '        Console.WriteLine("choose your path: 0-first room 1-library 2-billiards room")
+
+        '        interror = currentRoom
+        '        currentRoom = Console.ReadLine
+        '        If currentRoom = 0 Then
+        '            firstRoom.DisplayDescription()
+        '        ElseIf currentRoom = 1 Then
+        '            secondRoom.DisplayDescription()
+        '        ElseIf currentRoom = 2 Then
+        '            thirdRoom.DisplayDescription()
+        '        ElseIf currentRoom < 0 Then
+        '            Exit Do
+        '        Else
+        '            Console.WriteLine("Invalid input")
+        '            currentRoom = interror
+        '        End If
+        '    ElseIf input = 1 Then
+        '        'inventory
+        '    Else
+        '        'explore
+
+        '    End If
+
+        'Loop
+
+        'Console.WriteLine("Bye for now!")
+
+        Dim strMasterRoom(3, 9) As String
+
+        strMasterRoom(0, 0) = "The Study"                                'Room Name
+        strMasterRoom(0, 1) = "This is where procrastinating is done."   'Room Description Text
+        strMasterRoom(0, 2) = 0                                          'Lock Status. 0 = Unlocked. 1 = Locked
+        strMasterRoom(0, 3) = "You explore and find nothing of use"      'Explore Room Text
+        strMasterRoom(0, 4) = 2                                          'Indicates # of exits
+        strMasterRoom(0, 5) = 1                                          'Indicates Index of One Exit in Array - in this case the library
+        strMasterRoom(0, 6) = 2                                          'Indicates Index of One Exit in Array - in this case the billiards room
+
+        strMasterRoom(1, 0) = "The Library"                         'Room Name
+        strMasterRoom(1, 1) = "Where wild books make their nest."   'Room Description Text
+        strMasterRoom(1, 2) = 0                                     'Lock Status. 0 = Unlocked. 1 = Locked
+        strMasterRoom(1, 3) = "You discovered the Wrench"           'Explore Room Text
+        strMasterRoom(1, 4) = 2                                     'Indicates # of exits
+        strMasterRoom(1, 5) = 0                                     'Indicates Index of One Exit in Array - in this case the study
+        strMasterRoom(1, 6) = 2                                     'Indicates Index of One Exit in Array - in this case the billiards room
+
+        strMasterRoom(2, 0) = "The Billiards Room"                           'Room Name
+        strMasterRoom(2, 1) = "This has a pool table, not to be confused with a table inside of a pool."       'Room Description Text
+        strMasterRoom(2, 2) = 0                                     'Lock Status. 0 = Unlocked. 1 = Locked
+        strMasterRoom(2, 3) = "You explore and find a secret exit"  'Explore Room Text
+        strMasterRoom(2, 4) = 3                                     'Indicates # of exits
+        strMasterRoom(2, 5) = 0                                     'Indicates Index of One Exit in Array - in this case the study
+        strMasterRoom(2, 6) = 1                                     'Indicates Index of One Exit in Array - in this case the library
+        strMasterRoom(2, 7) = 3                                     'Indicates Index of One Exit in Array - in this case the ballroom
+
+        strMasterRoom(3, 0) = "The Ballroom"                        'Room Name
+        strMasterRoom(3, 1) = "Sadly, not a ballpit."               'Room Description Text
+        strMasterRoom(3, 2) = 0                                     'Lock Status. 0 = Unlocked. 1 = Locked
+        strMasterRoom(3, 3) = "You reached the exit"                'Explore Room Text
+        strMasterRoom(3, 4) = 2                                     'Indicates # of exits
+        strMasterRoom(3, 5) = 0                                     'Indicates Index of One Exit in Array
+        strMasterRoom(3, 6) = 2                                     'Indicates Index of One Exit in Array
+
+        Dim strRoomMotion As String
+        Dim nextRoom As Integer
+
+        currentRoom = 0
+        Do While currentRoom >= 0
+
+            'introductions to the room
+
+            Console.WriteLine("You are in the " & strMasterRoom(currentRoom, 0))
+            Console.WriteLine(strMasterRoom(currentRoom, 1))
+            Console.WriteLine()
+
+            'player choice
+
+            Console.WriteLine("what do you want to do: 0-move 1-inventory 2-explore")
+            input = Console.ReadLine
+
+            If input = 0 Then
+
+                'this block will customize the room options. the user will always type sequentially starting from 0. the text will be as long as it needs to be for all options to appear
+                strRoomMotion = "would you like to go to the " & strMasterRoom(strMasterRoom(currentRoom, 5), 0) & "(Option 0)"
+                For inti = 1 To (strMasterRoom(currentRoom, 4) - 1)
+                    strRoomMotion = strRoomMotion & " or the " & strMasterRoom(strMasterRoom(currentRoom, inti + 5), 0) & "(Option " & inti & ")"
+                Next
+
+                'outputs the text of the room options
+                Console.WriteLine(strRoomMotion)
+
+                'reads response and moves to correct room
+                'in future consider better error trapping if there are secret rooms, locked rooms, and rooms with more exit possibilities
+                nextRoom = Console.ReadLine
+                If nextRoom = 0 Then
+                    currentRoom = strMasterRoom(currentRoom, 5)
+                ElseIf nextRoom = 1 Then
+                    currentRoom = strMasterRoom(currentRoom, 6)
+                ElseIf currentRoom = 2 Then
+                    currentRoom = strMasterRoom(currentRoom, 7)
+                Else
+
+                    Console.WriteLine("Invalid Input")
+                End If
+
+            ElseIf input = 1 Then
+                'inventory
+            Else
+                'explore
+                Console.WriteLine(strMasterRoom(currentRoom, 3))
+
+                'in future consider how this step changes other parts. does this add something to your inventory? open up another room option and if so how does it get added to list
+
+            End If
+
+        Loop
+
+        Console.WriteLine("Bye for now!")
+        Console.ReadKey()
+
+    End Sub
+
+
+End Module
--- AllSpark/Module1.vb
+++ AllSpark/Module1.vb
@@ -1,27 +1,69 @@
 ﻿Module Module1
+    Dim currentRoom As Room
+    Dim input As Integer
+    Dim doorways(6) As Doorway
+    Dim rooms(5) As Room
+    Dim strRoomMotion As String
+    Dim nextRoom As Integer
+    Dim INVENTORYMAX As Integer = 4
+    Dim inventory(INVENTORYMAX) As Inventory
+    Dim currentItem As Inventory
+    Dim strItemList As String
+    Sub Main()
 
-	Sub Main()
+        Creation()
+        Do While currentRoom IsNot Nothing
+            Console.ForegroundColor = ConsoleColor.Gray
+            For inti = 0 To 2
+                For intj = 0 To 100
+                    Console.Write("*")
+                Next
+                Console.WriteLine()
+            Next
+            Console.WriteLine()
+            Console.ForegroundColor = ConsoleColor.Blue
+            Console.WriteLine("You are in the " & currentRoom.Name)
+            Console.WriteLine(currentRoom.Description)
+            Console.WriteLine()
+            Console.ForegroundColor = ConsoleColor.White
 
-		Dim currentRoom As Room
-		Dim input As Integer
+            Console.WriteLine("What do you want to do: 1-move 2-inventory 3-explore")
+            input = Val(Console.ReadLine)
+            If input = 1 Then
+                Movement()
+            ElseIf input = 2 Then
+                Backpack()
+            ElseIf input = 3 Then
+                Exploration()
+            Else
+                Console.WriteLine("Invalid")
+            End If
+        Loop
+        Console.WriteLine("Bye for now!")
+        Console.ReadKey()
+    End Sub
 
-        Dim rooms(3) As Room
+    Public Sub Creation()
         rooms(0) = New Room("The Study", "This is where procrastinating is done.", "You explore and find ", "The Candlestick") ' 1, 2, 3
-        rooms(1) = New Room("The Library", "Where wild books make their nest.", "You explore and find  ", "The Wrench") ' 0, 2
-        rooms(2) = New Room("The Billiards Room", "This has a pool table, not to be confused with a table inside of a pool.", "You explore and find a secret exit.", "") ' 0, 1, 3
+        rooms(1) = New Room("The Library", "Where wild books make their nest.", "You explore and find  ", "The Steel Key") ' 0, 2
+        rooms(2) = New Room("The Billiards Room", "This has a pool table, not to be confused with a table inside of a pool.", "You explore and find ", "a Secret Exit") ' 0, 1, 3
         rooms(3) = New Room("The Ballroom", "Sadly, not a ballpit.", "You reached the exit.", "") ' 0, 2
+        rooms(4) = New Room("The Kitchen", "Help yourself to the fridge", "You explore and find", "The Iron Key")
+        rooms(5) = New Room("LOCKED EXIT", "This is the end", "You explore and find", "The Lead Pipe")
 
-        Dim doorways(4) As Doorway
         doorways(0) = New Doorway(rooms(0), rooms(1), "A door", Nothing, Nothing, False) ' study to library
         doorways(1) = New Doorway(rooms(0), rooms(2), "A door", Nothing, Nothing, False) ' study to billiards
-        doorways(2) = New Doorway(rooms(0), rooms(3), "A door", "It is barricaded on one side", "Remove barricade", True) ' study to ballroom - secret passage
+        doorways(2) = New Doorway(rooms(0), rooms(3), "A door", "The Steel Door is Locked", "The Steel Key", True) ' study to ballroom - secret passage
         doorways(3) = New Doorway(rooms(1), rooms(2), "A door", Nothing, Nothing, False) ' library to billiards
         doorways(4) = New Doorway(rooms(2), rooms(3), "A door", Nothing, Nothing, False) ' billiards to ballroom
+        doorways(5) = New Doorway(rooms(0), rooms(4), "A cool door", Nothing, Nothing, False)
+        doorways(6) = New Doorway(rooms(4), rooms(5), "A locked Door", "The Iron Door is Locked", "The Iron Key", True)
 
-        ReDim rooms(0).Doorways(2) ' study
+        ReDim rooms(0).Doorways(3) ' study
         rooms(0).Doorways(0) = doorways(0)
         rooms(0).Doorways(1) = doorways(1)
         rooms(0).Doorways(2) = doorways(2)
+        rooms(0).Doorways(3) = doorways(5)
 
         ReDim rooms(1).Doorways(1) ' library
         rooms(1).Doorways(0) = doorways(0)
@@ -36,125 +78,84 @@
         rooms(3).Doorways(0) = doorways(2)
         rooms(3).Doorways(1) = doorways(4)
 
-        Dim strRoomMotion As String
-        Dim nextRoom As Integer
-        Dim intScreenReset As Integer
+        ReDim rooms(4).Doorways(0)
+        rooms(4).Doorways(0) = doorways(6)
+
         currentRoom = rooms(0)
 
-        Dim inventory(2) As Inventory
         inventory(0) = New Inventory(False, "The Lead Pipe", "A blunt weapon", 1)
         inventory(1) = New Inventory(False, "The Wrench", "Handy for plumbing", 1)
         inventory(2) = New Inventory(False, "The Candlestick", "Good for mood lighting", 1)
-        Dim currentItem As Inventory
-        Dim strItemList As String
+        inventory(3) = New Inventory(False, "The Iron Key", "Are you the keymaster?", 1)
+        inventory(4) = New Inventory(False, "The Steel Key", "Are you the keymaster?", 1)
+    End Sub
 
-        Do While currentRoom IsNot Nothing
+    Public Sub Movement()
+        strRoomMotion = "Move: Would you like to go to the " & currentRoom.Doorways(0).strGetOtherRoomName(currentRoom) & "(Option 1)"
+        For inti = 1 To (currentRoom.Doorways.Count - 1)
+            strRoomMotion = strRoomMotion & " or the " & currentRoom.Doorways(inti).strGetOtherRoomName(currentRoom) & "(Option " & (inti + 1) & ")"
+        Next
 
-            intScreenReset += 1
+        Console.WriteLine(strRoomMotion)
+        nextRoom = Val(Console.ReadLine)
 
-            If intScreenReset = 4 Then
-                Console.Clear()
-                intScreenReset = 0
-            End If
-
-            Console.ForegroundColor = ConsoleColor.Gray
-
-            For inti = 0 To 2
-                For intj = 0 To 100
-                    Console.Write("*")
-                Next
-                Console.WriteLine()
+        If nextRoom = 0 Then
+            Console.WriteLine("Invalid")
+        ElseIf currentRoom.Doorways(nextRoom - 1).Locked Then
+            Console.WriteLine(currentRoom.Doorways(nextRoom - 1).LockedDescription)
+            For intj = 0 To INVENTORYMAX
+                currentItem = inventory(intj)
+                If currentRoom.Doorways(nextRoom - 1).UnlockCondition = currentItem.ItemName And currentItem.Acquired = True Then
+                    Console.WriteLine("You use your " & currentItem.ItemName & " to open the door")
+                    currentRoom = currentRoom.Doorways(nextRoom - 1).roomGetOtherRoom(currentRoom)
+                    intj = INVENTORYMAX
+                End If
             Next
-
-            Console.WriteLine()
-
-            'introductions to the room
-
-            Console.ForegroundColor = ConsoleColor.Blue
-
-            Console.WriteLine("You are in the " & currentRoom.Name)
-			Console.WriteLine(currentRoom.Description)
-			Console.WriteLine()
-
-            Console.ForegroundColor = ConsoleColor.White
-
-            'player choice
-
-            Console.WriteLine("what do you want to do: 0-move 1-inventory 2-explore")
-			input = Console.ReadLine
-
-            If input = 0 Then
-
-                'this block will customize the room options. the user will always type sequentially starting from 0. the text will be as long as it needs to be for all options to appear
-                strRoomMotion = "Move: Would you like to go to the " & currentRoom.Doorways(0).strGetOtherRoomName(currentRoom) & "(Option 0)"
-                For inti = 1 To (currentRoom.Doorways.Count - 1)
-                    strRoomMotion = strRoomMotion & " or the " & currentRoom.Doorways(inti).strGetOtherRoomName(currentRoom) & "(Option " & inti & ")"
-                Next
-
-                'outputs the text of the room options
-                Console.WriteLine(strRoomMotion)
-
-                'reads response and moves to correct room
-                'in future consider better error trapping if there are secret rooms, locked rooms, and rooms with more exit possibilities
-                nextRoom = Console.ReadLine
-
-                If currentRoom.Doorways(nextRoom).Locked Then
-                    Console.WriteLine(currentRoom.Doorways(nextRoom).LockedDescription)
-                Else
-                    If nextRoom = 0 Then
-                        currentRoom = currentRoom.Doorways(nextRoom).roomGetOtherRoom(currentRoom)
-                    ElseIf nextRoom = 1 Then
-                        currentRoom = currentRoom.Doorways(nextRoom).roomGetOtherRoom(currentRoom)
-                    ElseIf currentRoom.Doorways.Length = 3 And nextRoom = 2 Then
-                        currentRoom = currentRoom.Doorways(nextRoom).roomGetOtherRoom(currentRoom)
-                    Else
-
-                        Console.WriteLine("Invalid Input")
-                    End If
-                    Console.WriteLine()
-                End If
-
-            ElseIf input = 1 Then
-                strItemList = "You currently possess: "
-                For inti = 0 To 2
-                    currentItem = inventory(inti)
-                    If currentItem.Acquired = True Then
-                        strItemList = strItemList & currentItem.ItemName & ", "
-                    End If
-                Next
-
-                If strItemList = "You currently possess: " Then
-                    Console.WriteLine("You currently possess no items.")
-
-                Else
-                    Console.WriteLine(strItemList)
-                    Console.WriteLine("Which would you like to use?")
-
-                    'need to work on this part
-
-                End If
-
-                'inventory
+        Else
+            If nextRoom >= 1 And nextRoom <= doorways.Length Then
+                currentRoom = currentRoom.Doorways(nextRoom - 1).roomGetOtherRoom(currentRoom)
             Else
-                'explore
-                Console.WriteLine(currentRoom.ExploreText & currentRoom.hiddenitem)
-                If currentRoom.hiddenitem <> "None" Then
-                    For intj = 0 To 2
-                        currentItem = inventory(intj)
-                        If currentRoom.hiddenitem = currentItem.ItemName Then
-                            currentItem.Acquired = True
-                            Console.WriteLine(currentItem.ItemDescription)
-                        End If
-                    Next
-                End If
+                Console.WriteLine("Invalid Input")
             End If
+            Console.WriteLine()
+        End If
 
-        Loop
+        If currentRoom.Name = "LOCKED EXIT" Then
+            Console.WriteLine(currentRoom.Description)
+            currentRoom = Nothing
+        End If
 
-		Console.WriteLine("Bye for now!")
-		Console.ReadKey()
+    End Sub
 
-	End Sub
+    Public Sub Backpack()
+        strItemList = "You currently possess: "
+        For inti = 0 To 2
+            currentItem = inventory(inti)
+            If currentItem.Acquired = True Then
+                strItemList = strItemList & currentItem.ItemName & ", "
+            End If
+        Next
+        If strItemList = "You currently possess: " Then
+            Console.WriteLine("You currently possess no items.")
+        Else
+            Console.WriteLine(Left(strItemList, (Len(strItemList) - 2)))
+        End If
+    End Sub
 
-
+    Public Sub Exploration()
+        If currentRoom.hiddenitem <> "None" Then
+            Console.WriteLine(currentRoom.ExploreText & currentRoom.hiddenitem)
+            For intj = 0 To INVENTORYMAX
+                currentItem = inventory(intj)
+                If currentRoom.hiddenitem = currentItem.ItemName Then
+                    currentItem.Acquired = True
+                    Console.WriteLine(currentItem.ItemDescription)
+                    currentRoom.ExploreText = ""
+                    currentRoom.hiddenitem = "None"
+                End If
+            Next
+        Else
+            Console.WriteLine("There is nothing to be found")
+        End If
+    End Sub
 End Module
